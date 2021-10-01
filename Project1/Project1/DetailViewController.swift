3b9a698aa2d8360618d0073347eeebcb4810f725
humeyrasahin7 <58955580+humeyrasahin7@users.noreply.github.com>
2021-10-01 21:59:46 +0300

    Initial commit

---
+++ Project1/Project1/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Hümeyra Şahin on 1.10.2021.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    var selectedImageIndex: Int?
+    var totalImages: Int?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            if let imageToLoad = selectedImage {
+                imageView.image = UIImage(named: imageToLoad)
+                if let total = totalImages{
+                    if let indexToLoad = selectedImageIndex {
+                        title = "Picture \(indexToLoad) of \(total)"
+                        navigationItem.largeTitleDisplayMode = .never
+                    }
+                }
+            }
+        
+
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+  
+}
