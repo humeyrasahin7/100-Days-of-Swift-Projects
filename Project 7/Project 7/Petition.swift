2dcdd985a79e12a4012cac8026838e989f8fe89f
humeyrasahin7 <58955580+humeyrasahin7@users.noreply.github.com>
2021-10-10 13:55:26 +0300

    Project 7 upload

---
+++ Project 7/Project 7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project 7
+//
+//  Created by Hümeyra Şahin on 9.10.2021.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project 7/Project 7/Petitions.swift
@@ -0,0 +1,14 @@
+//
+//  Petitions.swift
+//  Project 7
+//
+//  Created by Hümeyra Şahin on 9.10.2021.
+//
+
+import Foundation
+
+struct Petitions: Codable{
+    
+    var results: [Petition]
+    
+}
